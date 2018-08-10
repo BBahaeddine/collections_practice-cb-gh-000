5aae5153f0bf2d0821bdd07cf3c4e56ae9418678
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-10 08:27:27 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,6 +1,12 @@
 def sort_array_asc(array)
   array.sort{|item1, item2|
-  item1 <=> item2
-    
+    item1 <=> item2
   }
+end
+
+def sor_array_desc(array)
+  sortedArray = array.sort{|item1, item2|
+    item1 <=> item2
+  }
+  return sortedArray.reverse()
 end
