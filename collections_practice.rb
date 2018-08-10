72498abf8796f7e003c0b527be3b245fa0b0113c
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-10 08:29:20 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,3 +10,12 @@ def sort_array_desc(array)
   }
   return sortedArray.reverse()
 end
+
+def sort_array_char_count(array)
+  array.sort{|item1, item2|
+    if item1.length == item2.length
+      return 0
+    elsif item1.length > item2.length
+      return 1
+  }
+end
